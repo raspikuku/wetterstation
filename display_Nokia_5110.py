0e5721f9a655c19e3b6a9a509ad21511e990156b
raspikuku <raspikuku@gmail.com>
2015-10-04 14:56:15 -0500

    Add # Draw a white filled box to clear the image.

--- display_Nokia_5110.py
+++ display_Nokia_5110.py
@@ -7,6 +7,7 @@ import Image
 import ImageDraw
 import ImageFont
 
+
 class DisplayNokia5110:
 
     def __init__(self):
@@ -19,6 +20,8 @@ class DisplayNokia5110:
         # Hardware SPI usage:
         self.display = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
 
+        # Load default font.
+        self.font = ImageFont.load_default()
 
     def display_weather_values(self, date_time, temperature, humidity, pressure, light):
         # Initialize library.
@@ -28,9 +31,6 @@ class DisplayNokia5110:
         self.display.clear()
         self.display.display()
 
-        # Load default font.
-        font = ImageFont.load_default()
-
         # Create blank image for drawing.
         # Make sure to create image with mode '1' for 1-bit color.
         image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
@@ -38,12 +38,14 @@ class DisplayNokia5110:
         # Get drawing object to draw on image.
         draw = ImageDraw.Draw(image)
 
+        # Draw a white filled box to clear the image.
+        draw.rectangle((0, 0, LCD.LCDWIDTH, LCD.LCDHEIGHT), outline=255, fill=255)
+
         # Write some text.
-        draw.text((0, 12), 'HEY =;)', font=font)
-        #draw.text((0, 12), date_time, font=self.font)
-        #draw.text((0, 24), "{:.1f}C ".format(temperature) + "{:.1f}%".format(humidity), font=self.font)
-        #draw.text((0, 36), "{:.2f}mBar".format(pressure), font=self.font)
-        #draw.text((0, 48), "{:.1f}lx".format(light), font=self.font)
+        draw.text((0, 12), date_time, font=self.font)
+        draw.text((0, 24), "{:.1f}C ".format(temperature) + "{:.1f}%".format(humidity), font=self.font)
+        draw.text((0, 36), "{:.2f}mBar".format(pressure), font=self.font)
+        draw.text((0, 48), "{:.1f}lx".format(light), font=self.font)
 
         # Display image.
         self.display.image(image)
