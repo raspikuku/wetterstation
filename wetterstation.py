17fae418564bf0973af2296c52ac12b45eaabd64
raspikuku <raspikuku@gmail.com>
2015-11-03 11:53:54 -0500

    Classify =;)

--- wetterstation.py
+++ wetterstation.py
@@ -5,94 +5,114 @@ from sensor_AM2302 import AM2302
 from thingspeak import Thingspeak
 from display_Nokia_5110 import DisplayNokia5110
 import RPi.GPIO as gpio
-import json
 from pprint import pprint
 import bmp180
 
+
+import json
 import time
 
-oConfig = open('config.json', 'r')
-config = json.loads(oConfig.read())
 
-light_sensor = BH1750()
-humid_sensor = AM2302(4)
-thingspeak = Thingspeak(config['thingspeak_api_key'], 'https://api.thingspeak.com/update')
-# display = DisplayNokia5110()
+class Wetterstation:
+    def __init__(self, key):
+        self.key = key
 
-light = light_sensor.read()
+        self.temperature = 0
+        self.temperature2 = 0
+        self.humidity = 0
+        self.pressure = 0
+        self.light = 0
 
-(temperature, humidity) = humid_sensor.read()
+        with open('config.json', 'r') as infile:
+            self.config = json.loads(infile.read())
 
-# So this here is the first (and maybe last) attempt of an error correction >>>
-if temperature > 40 or humidity > 110:
-    (temperature, humidity) = humid_sensor.read()
-# <<< Error correction end
+    def read_all(self):
+        self.read_sensors()
+        self.write_status()
+        #self.update_display()
+        self.check_alarm()
+        self.update_thingspeak()
 
-(temperature2, pressure) = bmp180.readBmp180(0x77)
+    def read_sensors(self):
+        light_sensor = BH1750()
+        humid_sensor = AM2302(4)
+        self.light = light_sensor.read()
+
+        (self.temperature, self.humidity) = humid_sensor.read()
+
+        # So this here is the first (and maybe last) attempt of an error correction >>>
+        if self.temperature > 40 or self.humidity > 110:
+            (self.temperature, self.humidity) = humid_sensor.read()
+        # <<< Error correction end
+
+        (self.temperature2, self.pressure) = bmp180.readBmp180(0x77)
+
+    def update_thingspeak(self):
+        thingspeak = Thingspeak(self.config['thingspeak_api_key'], 'https://api.thingspeak.com/update')
+        # a
+        # Update Thingspeak
+
+        result = thingspeak.send_data(self.temperature2, self.humidity, self.pressure, self.light)
+
+        print result
+
+    def write_status(self):
+        date_time = time.strftime("%d-%m-%Y,%H:%M:%S")
+
+        json_string = '{"datetime":"' + date_time + '",'
+        json_string = json_string + '"temp":"' + "{:.1f}".format(self.temperature2) + '",'
+        json_string = json_string + '"humid":"' + "{:.1f}".format(self.humidity) + '",'
+        json_string = json_string + '"press":"' + "{:.1f}".format(self.pressure) + '",'
+        json_string = json_string + '"light":"' + "{:.1f}".format(self.light) + '"'
+        json_string = json_string + '}'
+
+        print json_string
+
+        target = open('current_weather.json', 'w')
+        target.truncate()
+        target.write(json_string)
+        target.close()
+
+        jdata = json.loads(json_string)
+        pprint(jdata)
+
+    def update_display(self):
+        time_now = time.strftime("%d-%m    %H:%M")
+        display = DisplayNokia5110()
+        display.display_weather_values(time_now, self.temperature2, self.humidity, self.pressure, self.light)
+        led_pin = 18
+
+        #gpio.setmode(gpio.BOARD)
+        gpio.setmode(gpio.BCM)
+
+        gpio.setup(led_pin, gpio.OUT)
+
+        for i in range(0, 3):
+            gpio.output(led_pin, gpio.LOW)
+            time.sleep(1)
+            gpio.output(led_pin, gpio.HIGH)
+            time.sleep(0.5)
+
+    def check_alarm(self):
+        temp = int(self.temperature2)
+        print temp
+
+        # Temperature alarm!
+        if temp > self.config['temp_alarm_value'] and self.config['temp_alarm_status'] == 1:
+            print "ALARM!!!"
+            gpio.setup(17, gpio.OUT)
+
+            for num in range(0, 10):
+                gpio.output(17, 0)
+                time.sleep(.3)
+                gpio.output(17, 1)
+                time.sleep(.3)
+
+            gpio.output(17, 0)
+
+        gpio.cleanup()
 
 
-date_time = time.strftime("%d-%m-%Y,%H:%M:%S")
-time_now = time.strftime("%d-%m    %H:%M")
+wetterstation = Wetterstation('foo')
 
-log = date_time + ","
-log = log + "{:.1f}C".format(temperature2) + ","
-log = log + "{:.1f}%".format(humidity) + ","
-log = log + "{:.2f}mBar".format(pressure) + ","
-log = log + "{:.1f}lx".format(light) + ","
-
-print log
-
-json_string = '{"datetime":"' + date_time + '",'
-json_string = json_string + '"temp":"' + "{:.1f}".format(temperature2) + '",'
-json_string = json_string + '"humid":"' + "{:.1f}".format(humidity) + '",'
-json_string = json_string + '"press":"' + "{:.1f}".format(pressure) + '",'
-json_string = json_string + '"light":"' + "{:.1f}".format(light) + '"'
-json_string = json_string + '}'
-
-print json_string
-
-target = open('current_weather.json', 'w')
-target.truncate()
-target.write(json_string)
-target.close()
-
-jdata = json.loads(json_string)
-pprint(jdata)
-#display.display_weather_values(time_now, temperature2, humidity, pressure, light)
-
-led_pin = 18
-
-#gpio.setmode(gpio.BOARD)
-gpio.setmode(gpio.BCM)
-
-gpio.setup(led_pin, gpio.OUT)
-
-for i in range(0, 3):
-    gpio.output(led_pin, gpio.LOW)
-    time.sleep(1)
-    gpio.output(led_pin, gpio.HIGH)
-    time.sleep(0.5)
-
-temp = int(temperature2)
-print temp
-
-# Temperature alarm!
-if temp > config['temp_alarm_value'] and config['temp_alarm_status'] == 1:
-    print "ALARM!!!"
-    gpio.setup(17, gpio.OUT)
-
-    for num in range(0, 10):
-        gpio.output(17, 0)
-        time.sleep(.3)
-        gpio.output(17, 1)
-        time.sleep(.3)
-
-    gpio.output(17, 0)
-
-gpio.cleanup()
-
-# Update Thingspeak
-
-result = thingspeak.send_data(temperature2, humidity, pressure, light)
-
-print result
+wetterstation.read_all()
